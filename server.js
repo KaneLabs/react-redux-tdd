54e2d4d459fa687fdfb5f7d4f6040784e3de966e
KaneLabs <ryankane28@gmail.com>
2016-10-03 10:41:30 -0600

    added route

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path')
 
 app.use(function(req, res, next) {
-
-
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if(req.connection.encrypted){
-    res.send('Do to The API in use, this app only works on http connections');
-  };
   next();
 });
 
 app.use(express.static(__dirname + '/'));
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
 
 app.listen(process.env.PORT || 8080);
