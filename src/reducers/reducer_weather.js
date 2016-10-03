5e68b118f69823f23d8167d7bd5a87ae55feae79
KaneLabs <ryankane28@gmail.com>
2016-10-02 23:48:23 -0600

    fixed bug with weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,12 @@
-import { FETCH_WEATHER } from '../actions/fetch_weather';
+import { FETCH_WEATHER } from '../actions/types';
 
 export default function(state = [], action ){
 
   switch (action.type) {
     case FETCH_WEATHER:
       return [ ...state, action.payload.data ];
+    default:
+    return state;
   }
-  return state
+  
 }
