be1ae75a9ed0ddaad27148b939af6e9ce80876da
KaneLabs <ryankane28@gmail.com>
2016-10-03 01:34:35 -0600

    api call must be http

--- src/actions/action_fetch_weather.js
+++ src/actions/action_fetch_weather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_WEATHER } from './action_types'
 
 const API_KEY = '60680212f39e06a473f9f1196643ffd3';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
