96016db9e2aae3924673f622fc4c7e0916a7eb1f
KaneLabs <ryankane28@gmail.com>
2016-10-03 10:37:52 -0600

    error handling for https connection

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res, next) {
+
+
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if(req.connection.encrypted){
+    res.send('Do to The API in use, this app only works on http connections');
+  };
   next();
 });
 
